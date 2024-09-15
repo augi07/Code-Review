44715db54bd48bb2c3eea4f4e7ad384530e9bad3
augi07 <102184778+augi07@users.noreply.github.com>
2024-09-15 15:20:39 +0200

    Hinzufügen der Funktionen

---
+++ functions.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    const re = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+    return re.test(password);
+}
